7282e5bac71e8eaee0adaa154931d798050a1a15
Teddy721 <up2121599@myport.ac.uk>
2025-03-25 12:30:32 +0000

    Update mathmanager.dart

--- lib/mathmanager.dart
+++ lib/mathmanager.dart
@@ -19,40 +19,4 @@ double calculateMonthlyInterest(double amount, int years) {
     double monthlyRate = annualRate / 100 / 12;
     return amount * monthlyRate;
   }
-
-double calculateTax(double income) {
-    if (income <= 12570) {
-      return 0;
-    } else if (income <= 50270) {
-      return (income - 12570) * 0.2;
-    } else if (income <= 125140) {
-      return (50270 - 12570) * 0.2 + (income - 50270) * 0.4;
-    } else {
-      return (50270 - 12570) * 0.2 + (125140 - 50270) * 0.4 + (income - 125140) * 0.45;
-    }
-  }
-
-String calculateDegreeClassification(List<Map<String, dynamic>> modules) {
-    modules.sort((a, b) => a['mark'].compareTo(b['mark']));
-    List<Map<String, dynamic>> filteredModules = modules.sublist(1);
-
-    double totalMarks = 0;
-    int totalCredits = 0;
-    for (var module in filteredModules) {
-      totalMarks += module['mark'] * module['credits'];
-      totalCredits += module['credits'];
-    }
-
-    double average = totalMarks / totalCredits;
-    if (average >= 70) {
-      return 'First Class';
-    } else if (average >= 60) {
-      return 'Upper Second Class (2:1)';
-    } else if (average >= 50) {
-      return 'Lower Second Class (2:2)';
-    } else if (average >= 40) {
-      return 'Third Class';
-    } else {
-      return 'Fail';
-    }
-  }
+}
